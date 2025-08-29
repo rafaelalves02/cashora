8f2d6132b8dcb7780d8c847e561ca5e3cf66203d
Rafael Alves <rafaelalvesdecarvalho25@gmail.com>
2025-08-29 19:31:51 -0300

    subindo projeto web primeira versão

---
+++ Cashora.Web/Cashora.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cashora.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
